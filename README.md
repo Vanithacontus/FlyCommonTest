40062a8996984874e27ab216a7aecab878cf1938
Vanitha <vanitha.g@contus.in>
2023-02-02 22:30:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FlyCommonTestFramework
+
+A description of this package.
---
+++ Sources/FlyCommonTestFramework/FlyCommonTestFramework.swift
@@ -0,0 +1,6 @@
+public struct FlyCommonTestFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
